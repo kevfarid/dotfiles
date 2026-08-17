b439ec2a2d6456e8fae307aa4cac05cff7548ceb
Kevin Cruz <kevfaridc@gmail.com>
2026-08-17 02:41:22 -0500

    feat: tmux window forwarding from Ghostty, nvim tweaks, zed updates
    - Ghostty forwards cmd+t/ctrl+tab/ctrl+shift+tab to tmux prefix binds for window management
    - tmux: add explicit prefix bindings for new/next/previous window
    - nvim: dedupe file finder into project_files(), add Cmd+P, use filesystem toggle for Neotree
    - nvim: bump plugin lockfile versions
    - zed: add opencode agent server, switch dark theme to Tokyo Night
    - gitignore Zed's local prompts library db

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,27 +3,32 @@
 -- Add any additional keymaps here
 
 local map = vim.keymap.set
+local function project_files()
+  local cwd = vim.fn.getcwd()
+  if LazyVim and type(LazyVim.root) == "function" then
+    cwd = LazyVim.root()
+  end
+
+  if Snacks and Snacks.picker then
+    Snacks.picker.files({
+      cwd = cwd,
+      hidden = true,
+      ignored = true,
+    })
+  else
+    vim.cmd("find .")
+  end
+end
 
 -- Zed-like keymaps (normal mode) to mirror your workflow.
 map("n", "<Tab>", "<cmd>bnext<cr>", { desc = "Next buffer" })
 map("n", "<S-Tab>", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
-map("n", "<C-B>", "<cmd>Neotree toggle left<cr>", { desc = "Toggle left dock" })
+map("n", "<C-B>", "<cmd>Neotree filesystem toggle left<cr>", { desc = "Toggle left dock" })
 map("n", "<leader>w", "<cmd>w<cr>", { desc = "Save" })
 map("n", "<leader>q", "<cmd>bd<cr>", { desc = "Close active buffer" })
-map("n", "<leader><space>", function()
-  if Snacks and Snacks.picker then
-    Snacks.picker.files()
-  else
-    vim.cmd("find .")
-  end
-end, { desc = "File finder" })
-map("n", "<C-p>", function()
-  if Snacks and Snacks.picker then
-    Snacks.picker.files()
-  else
-    vim.cmd("find .")
-  end
-end, { desc = "VSCode: Quick Open" })
+map("n", "<leader><space>", project_files, { desc = "File finder" })
+map("n", "<C-p>", project_files, { desc = "VSCode: Quick Open" })
+map({ "n", "i" }, "<D-p>", project_files, { desc = "macOS: Quick Open" })
 map({ "n", "i" }, "<C-s>", "<cmd>w<cr>", { desc = "VSCode: Save" })
 map("n", "<C-w>", "<cmd>bd<cr>", { desc = "VSCode: Close editor" })
 
